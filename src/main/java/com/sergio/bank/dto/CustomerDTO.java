3557b4b2fcbf8e199d260558d2f3648cb11ebfaf
Sergio Ismael Ayala Hernandez <sergio.ayala@sofka.local>
2025-02-05 15:32:07 -0500

    se corrigen versiones, se agrega cambio a archivo dockerfile

--- src/main/java/com/sergio/bank/dto/CustomerDTO.java
+++ src/main/java/com/sergio/bank/dto/CustomerDTO.java
@@ -26,39 +26,11 @@ public class CustomerDTO {
     @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
     private String password;
 
-    public CustomerDTO(String name, String email, String documentNumber, String password) {
-        this.name = name;
-        this.email = email;
-        this.documentNumber = documentNumber;
-        this.password = password;
-    }
-
     public CustomerDTO() {
 
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getDocumentNumber() {
@@ -75,5 +47,9 @@ public class CustomerDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
