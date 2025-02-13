ff229d5ad418583d48c27eb533e0d4e87fac5487
StevenAngelSofka <alvaro.angel@sofka.com.co>
2025-02-13 13:26:18 -0500

    se corrigen test unitarios

--- src/main/java/com/sergio/bank/dto/CustomerDTO.java
+++ src/main/java/com/sergio/bank/dto/CustomerDTO.java
@@ -1,15 +1,49 @@
 package com.sergio.bank.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CustomerDTO {
     private Long id;
     private String name;
     private String email;
     private String documentNumber;
+
+    public CustomerDTO() {}
+
+    public CustomerDTO(Long id, String name, String email, String documentNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.documentNumber = documentNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
 }
--- src/main/java/com/sergio/bank/util/MessageConstants.java
+++ src/main/java/com/sergio/bank/util/MessageConstants.java
@@ -4,12 +4,11 @@ public class MessageConstants {
     private MessageConstants() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
     }
-    public static final String ERROR_EXIST_EMAIL = "El correo electrónico ya está registrado";
     public static final String ERROR_CUSTOMER_NOT_FOUND = "Customer not found with id: ";
     public static final String ACCOUNT_TYPE_SAVINGS = "SAVINGS";
     public static final String ACCOUNT_TYPE_CHECKING = "CHECKING";
     public static final String ERROR_INVALID_ACCOUNT_TYPE = "Invalid account type: ";
     public static final String ERROR_ACCOUNT_NOT_FOUND = "Account not found for ID: %d";
-    public static final String ERROR_ACCOUNTS_NOT_FOUND_BY_CUSTOMER = "No se encontraron cuentas asociadas al cliente con el id: ";
+    public static final String ERROR_ACCOUNTS_NOT_FOUND_BY_CUSTOMER = "No se encontraron cuentas asociadas al cliente con el id: %s";
 
 }
